42a0f388fded59118b08c4178825a5ed6bc0c3e6
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2020-09-02 11:10:54 +0300

    Revert "* Removed Country and Currency enums"
    This reverts commit 6d7e3f3f3571161c73ea2c258f99885497d6568e.

--- src/Mews.Fiscalization.Greece/Dto/Xsd/InvoiceHeader.cs
+++ src/Mews.Fiscalization.Greece/Dto/Xsd/InvoiceHeader.cs
@@ -26,7 +26,10 @@ namespace Mews.Fiscalization.Greece.Dto.Xsd
         public bool VatPaymentSuspensionSpecified { get; set; }
 
         [XmlElement(ElementName = "currency")]
-        public string CurrencyCode { get; set; }
+        public Currency Currency { get; set; }
+
+        [XmlIgnore]
+        public bool CurrencySpecified { get; set; }
 
         [XmlElement(ElementName = "exchangeRate")]
         public decimal ExchangeRate { get; set; }
