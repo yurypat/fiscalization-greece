26102c3c9d610cf1af6434f753653dab346b1b2e
Yury Patlyakevich <Yury.Patlyakevich@dataart.com>
2020-09-16 10:33:33 +0300

    Add tests

--- src/Mews.Fiscalization.Greece.Tests/IntegrationTests/AadeTestInvoicesData.cs
+++ src/Mews.Fiscalization.Greece.Tests/IntegrationTests/AadeTestInvoicesData.cs
@@ -20,9 +20,14 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
                 {
                     new object[] { SimpleValidInvoice() },
                     new object[] { InvoiceWithEmptyCounterpart() },
-                    new object[] { InvoiceWithDomesticCompanyCounterpart() },
-                    new object[] { InvoiceWithForeignCompanyCounterpart("CZ", BillType.SalesInvoiceIntraCommunitySupplies, ClassificationType.IntraCommunityForeignSalesOfGoodsAndServices) },
-                    new object[] { InvoiceWithForeignCompanyCounterpart("US", BillType.SalesInvoiceThirdCountrySupplies, ClassificationType.ThirdCountryForeignSalesOfGoodsAndServices) },
+                    new object[] { InvoiceWithDomesticCompanyCounterpart(PaymentType.Cash) },
+                    new object[] { InvoiceWithForeignCompanyCounterpart("CZ", BillType.SalesInvoiceIntraCommunitySupplies, ClassificationType.IntraCommunityForeignSalesOfGoodsAndServices, PaymentType.Cash) },
+                    new object[] { InvoiceWithForeignCompanyCounterpart("US", BillType.SalesInvoiceThirdCountrySupplies, ClassificationType.ThirdCountryForeignSalesOfGoodsAndServices, PaymentType.Cash) },
+                    new object[] { InvoiceWithDomesticCompanyCounterpart(PaymentType.OnCredit) },
+                    new object[] { InvoiceWithForeignCompanyCounterpart("CZ", BillType.SalesInvoiceIntraCommunitySupplies, ClassificationType.IntraCommunityForeignSalesOfGoodsAndServices, PaymentType.OnCredit) },
+                    new object[] { InvoiceWithForeignCompanyCounterpart("US", BillType.SalesInvoiceThirdCountrySupplies, ClassificationType.ThirdCountryForeignSalesOfGoodsAndServices, PaymentType.OnCredit) },
+                    new object[] { SimplifiedInvoiceForCustomer() },
+                    new object[] { InvoiceWithConversionRate() },
                     new object[] { InvoiceWithVariousOrderItemTypes() }
                 };
         }
@@ -60,7 +65,7 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
         }
 
         /// <summary>
-        /// Test case for bills: #1.2, 2.4, 2.5
+        /// Test case for bills: #1.2, #1.3, #2.4, #2.5
         /// </summary>
         private static InvoiceDocument InvoiceWithEmptyCounterpart()
         {
@@ -93,9 +98,9 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
         }
 
         /// <summary>
-        /// Test case for bills: #1.3 with domestic counterpart
+        /// Test case for bills: #1.4 with domestic counterpart(payment type: cash) and #1.4 with domestic counterpart(with payment type: onCredit)
         /// </summary>
-        private static InvoiceDocument InvoiceWithDomesticCompanyCounterpart()
+        private static InvoiceDocument InvoiceWithDomesticCompanyCounterpart(PaymentType paymentType)
         {
             return new InvoiceDocument(
                 new List<InvoiceRecord>()
@@ -103,32 +108,30 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
                     new InvoiceRecord(null, null, null,
                         new InvoiceRecordParty(new NotEmptyString(UserVatNumber), new NonNegativeInt(0), null, new CountryCode("GR"), null),
                         new InvoiceRecordParty(new NotEmptyString("090701500"), new NonNegativeInt(0), null, new CountryCode("GR"), null),                        
-                        // "InvoiceType": "Sales"
                         new InvoiceRecordHeader(new LimitedString1to50("0"), new LimitedString1to50("50020"), DateTime.Now, BillType.SalesInvoice, new CurrencyCode("EUR"), null),
                         new List<InvoiceRecordPaymentMethodDetails>
                         {
-                            // "PaymentType": "Cash"
-                            new InvoiceRecordPaymentMethodDetails(new Amount(100m), PaymentType.Cash)
+                            new InvoiceRecordPaymentMethodDetails(new Amount(100m), paymentType)
                         },
                         new List<InvoiceRecordDetail>
                         {
                             new InvoiceRecordDetail(new PositiveInt(1), new Amount(88.50m), TaxType.Vat13, null, new Amount(11.50m), null, new List<InvoiceRecordIncomeClassification>
                             {
-                                new InvoiceRecordIncomeClassification(ClassificationType.OtherSalesOfGoodsAndServices, ClassificationCategory.ProductSaleIncome, new Amount(88.50m))
+                                new InvoiceRecordIncomeClassification(ClassificationType.OtherSalesOfGoodsAndServices, ClassificationCategory.ProvisionOfServicesIncome, new Amount(88.50m))
                             })
                         },
                         new InvoiceRecordSummary(new Amount(88.50m),new Amount(11.50m), new Amount(100m),new List<InvoiceRecordIncomeClassification>
                         {
-                            new InvoiceRecordIncomeClassification(ClassificationType.OtherSalesOfGoodsAndServices, ClassificationCategory.ProductSaleIncome, new Amount(88.50m))
+                            new InvoiceRecordIncomeClassification(ClassificationType.OtherSalesOfGoodsAndServices, ClassificationCategory.ProvisionOfServicesIncome, new Amount(88.50m))
                         })
                     )
                 });
         }
 
         /// <summary>
-        /// Test case for bills: #1.3 with foreign counterpart
+        /// Test case for bills: #1.4 with foreign counterpart(payment type: cash) and #1.4 with foreign counterpart(with payment type: onCredit)
         /// </summary>
-        private static InvoiceDocument InvoiceWithForeignCompanyCounterpart(string countryCode, BillType billType, ClassificationType classificationType)
+        private static InvoiceDocument InvoiceWithForeignCompanyCounterpart(string countryCode, BillType billType, ClassificationType classificationType, PaymentType paymentType)
         {
             return new InvoiceDocument(
                 new List<InvoiceRecord>()
@@ -136,12 +139,10 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
                     new InvoiceRecord(null, null, null,
                         new InvoiceRecordParty(new NotEmptyString(UserVatNumber), new NonNegativeInt(0), null, new CountryCode("GR"), null),
                         new InvoiceRecordParty(new NotEmptyString("12348765"), new NonNegativeInt(0), new StringIdentifier("Name"), new CountryCode(countryCode), new InvoiceRecordPartyAddress(null, null, postalCode: new NotEmptyString("12"), city: new NotEmptyString("City"))),                       
-                        // "InvoiceType": "Sales"
                         new InvoiceRecordHeader(new LimitedString1to50("0"), new LimitedString1to50("50020"), DateTime.Now, billType, new CurrencyCode("EUR"), null),
                         new List<InvoiceRecordPaymentMethodDetails>
                         {
-                            // "PaymentType": "Cash"
-                            new InvoiceRecordPaymentMethodDetails(new Amount(100m), PaymentType.Cash)
+                            new InvoiceRecordPaymentMethodDetails(new Amount(100m), paymentType)
                         },
                         new List<InvoiceRecordDetail>
                         {
@@ -153,6 +154,72 @@ namespace Mews.Fiscalization.Greece.Tests.IntegrationTests
                         new InvoiceRecordSummary(new Amount(100m),new Amount(0m), new Amount(100m),new List<InvoiceRecordIncomeClassification>
                         {
                             new InvoiceRecordIncomeClassification(classificationType, ClassificationCategory.ProvisionOfServicesIncome, new Amount(100m))
+                        })
+                    )
+                });
+        }
+
+        /// <summary>
+        /// Test case for bills: #1.5
+        /// </summary>
+        private static InvoiceDocument SimplifiedInvoiceForCustomer()
+        {
+            return new InvoiceDocument(
+                new List<InvoiceRecord>()
+                {
+                    new InvoiceRecord(null, null, null,
+                        new InvoiceRecordParty(new NotEmptyString(UserVatNumber), new NonNegativeInt(0), null, new CountryCode("GR"), null),
+                        null,
+                        new InvoiceRecordHeader(new LimitedString1to50("0"), new LimitedString1to50("50020"), DateTime.Now, BillType.SimplifiedInvoice, new CurrencyCode("EUR"), null),
+                        new List<InvoiceRecordPaymentMethodDetails>
+                        {
+                            new InvoiceRecordPaymentMethodDetails(new Amount(100m), PaymentType.OnCredit)
+                        },
+                        new List<InvoiceRecordDetail>
+                        {
+                            new InvoiceRecordDetail(new PositiveInt(1), new Amount(88.50m), TaxType.Vat13, null, new Amount(11.50m), null, new List<InvoiceRecordIncomeClassification>
+                            {
+                                new InvoiceRecordIncomeClassification(ClassificationType.RetailSalesOfGoodsAndServicesPrivateClientele, ClassificationCategory.ProductSaleIncome, new Amount(88.50m))
+                            })
+                        },
+                        new InvoiceRecordSummary(new Amount(88.50m),new Amount(11.50m), new Amount(100m),new List<InvoiceRecordIncomeClassification>
+                        {
+                            new InvoiceRecordIncomeClassification(ClassificationType.RetailSalesOfGoodsAndServicesPrivateClientele, ClassificationCategory.ProductSaleIncome, new Amount(88.50m))
+                        })
+                    )
+                });
+        }
+
+        /// <summary>
+        /// Test case for bills: #1.6
+        /// </summary>
+        private static InvoiceDocument InvoiceWithConversionRate()
+        {
+            return new InvoiceDocument(
+                new List<InvoiceRecord>()
+                {
+                    new InvoiceRecord(null, null, null,
+                        new InvoiceRecordParty(new NotEmptyString(UserVatNumber), new NonNegativeInt(0), null, new CountryCode("GR"), null),
+                        null,
+                        new InvoiceRecordHeader(new LimitedString1to50("0"), new LimitedString1to50("50020"), DateTime.Now, BillType.RetailSalesReceipt, new CurrencyCode("EUR"), null),
+                        new List<InvoiceRecordPaymentMethodDetails>
+                        {
+                            new InvoiceRecordPaymentMethodDetails(new Amount(10m), PaymentType.Cash)
+                        },
+                        new List<InvoiceRecordDetail>
+                        {
+                            new InvoiceRecordDetail(new PositiveInt(1), new Amount(4.03m), TaxType.Vat24, null, new Amount(0.97m), null, new List<InvoiceRecordIncomeClassification>
+                            {
+                                new InvoiceRecordIncomeClassification(ClassificationType.RetailSalesOfGoodsAndServicesPrivateClientele, ClassificationCategory.ProductSaleIncome, new Amount(4.03m))
+                            }),
+                            new InvoiceRecordDetail(new PositiveInt(2), new Amount(4.03m), TaxType.Vat24, null, new Amount(0.97m), null, new List<InvoiceRecordIncomeClassification>
+                            {
+                                new InvoiceRecordIncomeClassification(ClassificationType.RetailSalesOfGoodsAndServicesPrivateClientele, ClassificationCategory.ProductSaleIncome, new Amount(4.03m))
+                            })
+                        },
+                        new InvoiceRecordSummary(new Amount(8.06m),new Amount(1.94m), new Amount(10m),new List<InvoiceRecordIncomeClassification>
+                        {
+                            new InvoiceRecordIncomeClassification(ClassificationType.RetailSalesOfGoodsAndServicesPrivateClientele, ClassificationCategory.ProductSaleIncome, new Amount(8.06m))
                         })
                     )
                 });
