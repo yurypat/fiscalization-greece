db8961e3f243a8647769b24d9a340b7a0068e7b1
abdallahbeshi <abdallah.altrabeishi@mews.com>
2020-09-22 19:27:00 +0200

    Updated library version.

--- src/Mews.Fiscalization.Greece/RestClient.cs
+++ src/Mews.Fiscalization.Greece/RestClient.cs
@@ -116,7 +116,7 @@ namespace Mews.Fiscalization.Greece
                                 Message = errorMessage
                             }
                         }
-                    }).ToArray()
+                }).ToArray()
             };
         }
     }
