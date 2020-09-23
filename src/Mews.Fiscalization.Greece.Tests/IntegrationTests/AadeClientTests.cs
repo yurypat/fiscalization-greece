1610d419077f1fb9ac4993a7ec6319c541b54a31
Yury Patlyakevich <Yury.Patlyakevich@dataart.com>
2020-09-23 18:05:32 +0300

    Add new lines

--- src/Mews.Fiscalization.Greece.Tests/IntegrationTests/AadeClientTests.cs
+++ src/Mews.Fiscalization.Greece.Tests/IntegrationTests/AadeClientTests.cs
@@ -1,7 +1,5 @@
 ﻿using Mews.Fiscalization.Greece.Model;
-using Mews.Fiscalization.Greece.Model.Types;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
